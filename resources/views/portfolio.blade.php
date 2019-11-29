1d89abea7978eeef0e98fb9fe856c60670e0fb3f
Amir Syafiq <amirsyafiqmohdrizuan@gmail.com>
2019-11-30 00:42:39 +0800

    My Portfolio

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>My Portfolio</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    <link href="DevFolio/img/favicon.png" rel="icon">
+    <link href="DevFolio/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Bootstrap CSS File -->
+    <link href="DevFolio/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="DevFolio/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="DevFolio/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="DevFolio/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="DevFolio/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="DevFolio/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="DevFolio/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+      Theme Name: DevFolio
+      Theme URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+      Author: BootstrapMade.com
+      License: https://bootstrapmade.com/license/
+    ======================================================= -->
+</head>
+
+<body id="page-top">
+
+<!--/ Nav Star /-->
+<nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll" href="#page-top">AMIRSMR</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+                aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </div>
+</nav>
+<!--/ Nav End /-->
+
+<!--/ Intro Skew Star /-->
+<div id="home" class="intro route bg-image" style="background-image: url(DevFolio/img/3.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+        <div class="table-cell">
+            <div class="container">
+                <!--<p class="display-6 color-d">Hello, world!</p>-->
+                <h1 class="intro-title mb-4">I am Amir Syafiq</h1>
+                <p class="intro-subtitle"><span class="text-slider-items">Programmer, Web Developer</span><strong class="text-slider"></strong></p>
+                <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+            </div>
+        </div>
+    </div>
+</div>
+<!--/ Intro Skew End /-->
+
+<section id="about" class="about-mf sect-pt4 route">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="box-shadow-full">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-5">
+                                    <div class="about-img">
+                                        <img src="DevFolio/img/AMIR.jpg" class="img-fluid rounded b-shadow-a" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-7">
+                                    <div class="about-info">
+                                        <p><span class="title-s">Name: </span> <span>Amir Syafiq B. Mohd Rizuan</span></p>
+                                        <p><span class="title-s">Profile: </span> <span>Programmer and Web Developer</span></p>
+                                        <p><span class="title-s">Email: </span> <span>amirsyafiqmohdrizuan@gmail.com</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="skill-mf">
+                                <p class="title-s">Skill</p>
+                                <span>HTML</span> <span class="pull-right">85%</span>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0"
+                                         aria-valuemax="100"></div>
+                                </div>
+                                <span>LARAVEL</span> <span class="pull-right">75%</span>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0"
+                                         aria-valuemax="100"></div>
+                                </div>
+                                <span>PHP</span> <span class="pull-right">80%</span>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                         aria-valuemax="100"></div>
+                                </div>
+                                <span>JAVASCRIPT</span> <span class="pull-right">50%</span>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0"
+                                         aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="about-me pt-4 pt-md-0">
+                                <div class="title-box-2">
+                                    <h5 class="title-left">
+                                        Curriculum Vitae
+                                    </h5>
+                                </div>
+                                <img src="DevFolio/img/CV.jpg" class="img-fluid rounded b-shadow-a" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="copyright-box">
+                        <p class="copyright">&copy; Copyright <strong>AMIRSMR</strong>. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
+<!--/ Section Contact-footer End /-->
+
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+<div id="preloader"></div>
+
+<!-- JavaScript Libraries -->
+<script src="DevFolio/lib/jquery/jquery.min.js"></script>
+<script src="DevFolio/lib/jquery/jquery-migrate.min.js"></script>
+<script src="DevFolio/lib/popper/popper.min.js"></script>
+<script src="DevFolio/lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="DevFolio/lib/easing/easing.min.js"></script>
+<script src="DevFolio/lib/counterup/jquery.waypoints.min.js"></script>
+<script src="DevFolio/lib/counterup/jquery.counterup.js"></script>
+<script src="DevFolio/lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="DevFolio/lib/lightbox/js/lightbox.min.js"></script>
+<script src="DevFolio/lib/typed/typed.min.js"></script>
+<!-- Contact Form JavaScript File -->
+<script src="DevFolio/contactform/contactform.js"></script>
+
+<!-- Template Main Javascript File -->
+<script src="DevFolio/js/main.js"></script>
+
+</body>
+</html>
